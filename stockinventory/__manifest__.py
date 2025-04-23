368468975976056c9737f340c23d2e760ce3b0a9
swati.khandelwal <swati.khandelwal@mail.codetrade.io>
2025-04-23 17:01:47 +0530

    Add images

--- stockinventory/__manifest__.py
+++ stockinventory/__manifest__.py
@@ -1,15 +1,16 @@
 {
     'name': 'Stock Aging Inventory', 
-    'version': '18.0.1', 
+    'version': '18.0.1.0.0', 
     'summary': """Stock Aging Report""",
     'description': """Stock Aging Report""",
     'author': "Swati Khandelwal" ,
-    'category': 'Swati Khandelwal',
+    'category': 'Excel Sheet',
     'depends': ['point_of_sale', 'stock'],
     'data': [
        'security/ir.model.access.csv', 
        'wizard/stock_wizard.xml',
     ],
+    'images' : 'static/description/banner.png',
     'installable': True,
     'auto_install': False,
     'license': 'LGPL-3',
