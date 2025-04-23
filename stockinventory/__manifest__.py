f908b35af56f424a71cd950e7fb11174e2ff3c3a
swati.khandelwal <swati.khandelwal@mail.codetrade.io>
2025-04-23 16:43:08 +0530

    Add code

--- stockinventory/__manifest__.py
+++ stockinventory/__manifest__.py
@@ -3,8 +3,8 @@
     'version': '18.0.1', 
     'summary': """Stock Aging Report""",
     'description': """Stock Aging Report""",
-    'author': "Codetrade" ,
-    'category': 'Category',
+    'author': "Swati Khandelwal" ,
+    'category': 'Swati Khandelwal',
     'depends': ['point_of_sale', 'stock'],
     'data': [
        'security/ir.model.access.csv', 
